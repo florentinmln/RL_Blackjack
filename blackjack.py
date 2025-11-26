735e76550dd8fbd4f55d511be90bd032bd3683b2
Clement Gaudissard <clement.gaudissard@univ-rouen.fr>
2025-11-26 18:54:20 +0100

    Fonction reset() faite.

--- blackjack.py
+++ blackjack.py
@@ -14,10 +14,30 @@ class BlackJackEnv(gym.Env):
         # Init de l'environnement, création des main pour les joueurs.
         # On passe en param la taille du deck, je ne sais pas si c'est pertinent mais bon je trouve ca cool.
 
-    def reset(self, seed: int = None):
+    def reset(self, seed: int):
         # To DO
         # A chaque fin de partie recréer une partie avec la seed pour gerer l'aleatoire.
 
+        # Important: permet de set la seed pour tout le random.
+        super().reset(seed=seed)
+
+        self.deck.reset()
+
+        self.player.score = 0
+        self.player.hand.append(self.deck.take_card(self.np_random.integers(0, self.deck.lenght())))
+        self.player.hand.append(self.deck.take_card(self.np_random.integers(0, self.deck.lenght())))
+        self.player.score_calcul()
+
+        self.dealer.score = 0
+        self.dealer.hand.append(self.deck.take_card(self.np_random.integers(0, self.deck.lenght())))
+        self.dealer.hand.append(self.deck.take_card(self.np_random.integers(0, self.deck.lenght())))
+        self.dealer.score_calcul()
+
+        obs = self._get_obs()
+        info = self._get_info()
+
+        return obs, info
+        
     def step(self, action):
         # To Do
         # Une boucle de jeu, c'est a dire une action qui donne une reward et un nouveau state.
